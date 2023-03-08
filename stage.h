fb17f1050f12fb87337ba261277fdbc6d5e1ec12
SurrealSky <40445799+SurrealSky@users.noreply.github.com>
2023-03-08 08:42:43 +0800

    更新

--- stage.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<Windows.h>
-#include<stdbool.h>
-
-void GetStageless(unsigned char *url,unsigned char** dst, unsigned int *size);
-
-bool DecryptStageless(unsigned char* buffer, unsigned int size);
